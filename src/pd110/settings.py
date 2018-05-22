57abe0c5e8ea6d46f3b81932c52bbd6ff7c2d6b1
Franchu Bernal <franchubernal@gmail.com>
2018-05-22 23:49:40 +0200

    Configurar Email para tu Página Web

--- src/pd110/settings.py
+++ src/pd110/settings.py
@@ -27,6 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'franchubernal@gmail.com'
+EMAIL_HOST_PASSWORD = 'Pirateking95!!'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+
 
 # Application definition
 
